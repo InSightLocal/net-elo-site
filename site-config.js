a447c5f4478a4bb39097cb5e185eece09ae9ac32
InSight Local <insight.local@icloud.com>
2026-08-29 08:56:51 +0200

    Net Elo : ajoute site-config.js

---
+++ site-config.js
@@ -0,0 +1,5 @@
+window.NET_ELO_CONFIG = {
+  // Identifiant Google Analytics 4.
+  // Le script Analytics ne se charge qu'après consentement via le bandeau cookies.
+  gaMeasurementId: "G-EXW1M6L10N"
+};
